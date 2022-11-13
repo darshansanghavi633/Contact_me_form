9dae1ba09bd86554abaf725151556c399d60c472
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2022-11-13 15:18:18 +0530

    basic form

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const contact = document.getElementById('contact')
+const email = document.getElementById('email')
+const errorElement = document.getElementById('error')
+form.addEventListener('submit', (e) => {
+    let messages = [] 
+    if ( username.value === '' || username.value == null ){
+        messages.push('Name is required\n')
+    }
+    if (contact.value.length != 10) {
+        messages.push('Conatct number should be of 10 digits\n')
+    }
+    if (password.value.length <= 8) {
+        messages.push('Password must be longer than 8 characters & shorter than 20 characters\n')   
+    }
+    if (password.value.length >= 20) {
+        messages.push('Password must be longer than 8 characters & shorter than 20 characters\n')   
+    }
+    if ( email.value ==='' || email.value == null ) {
+        messages.push('Please fill out email\n')
+    }
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join('')  
+    }
+})
